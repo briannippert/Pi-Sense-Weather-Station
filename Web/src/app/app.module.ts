bf63a52b6e24ccb0e3eadb64c0ebd978164ea307
Brian Nippert <briannippert@gmail.com>
2021-01-03 20:35:40 -0500

    Added empty components

--- Web/src/app/app.module.ts
+++ Web/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ChartsComponent } from './charts/charts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    ChartsComponent
   ],
   imports: [
     BrowserModule,
