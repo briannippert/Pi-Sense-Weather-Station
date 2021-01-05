56a454dd3983f1efe7421b2eba532bc671201a10
Brian Nippert <briannippert@gmail.com>
2021-01-04 21:16:04 -0500

    Started DB work

--- API/server.js
+++ API/server.js
@@ -2,7 +2,16 @@
 const express = require('express')
 const app = express()
 const port = 80
- 
+const sqlite3 = require('sqlite3').verbose();
+
+
+let db = new sqlite3.Database('../db/weather.db',(err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Connected to the weather database.');
+  });
+
 app.get('/', function (req, res) {
     res.sendFile("/home/pi/Desktop/weather/index.html")
 });
