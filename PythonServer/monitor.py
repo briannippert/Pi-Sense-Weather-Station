1814ce08303be89ccc83d42fcb15acfdbc5b0be2
Brian Nippert <briannippert@gmail.com>
2021-02-07 00:33:43 -0500

    Worked on converting python monitoring srcipt

--- PythonServer/monitor.py
+++ PythonServer/monitor.py
@@ -16,18 +16,23 @@ def create_connection(db_file):
         print(e)
     return conn
 
+def insertRecord(humidity, pressure, temperature):
+    print(humidity, temperature, pressure)
+
+def captureData():
+    humidity=sense.get_humidity()
+    temperature=sense.get_temperature()
+    pressure=sense.get_pressure()
+    # Round the values to one decimal place
+    humidity=round(humidity, 1)
+    temperature=round(temperature, 1)
+    pressure=round(pressure, 1)
+    humidity=humidity + 5
+    temperature = (temperature * 9/5) + 27
+    sense.clear()
+    insertRecord(humidity, pressure, temperature)
+
+
 while(True):
-  humidity=sense.get_humidity()
-  temperature=sense.get_temperature()
-  pressure=sense.get_pressure()
-  # Round the values to one decimal place
-  humidity=round(humidity, 1)
-  temperature=round(temperature, 1)
-  pressure=round(pressure, 1)
-  humidity=humidity + 5
-  temperature = (temperature * 9/5) + 27
-  sense.clear()
-  with open('humidity.csv', mode='a') as csvFile:
-      csvWriter = csv.writer(csvFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-      csvWriter.writerow([str(humidity),str(int(time.time()))+"000",str(temperature),str(pressure)])
-  time.sleep(10)
+    captureData()
+    time.sleep(10)
