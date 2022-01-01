2d685327961160662e84be59356ff722724c1845
Brian Nippert <briannippert@gmail.com>
2021-12-31 22:57:27 -0500

    Started work on navbar

--- WeatherStation/ClientApp/src/App.js
+++ WeatherStation/ClientApp/src/App.js
@@ -14,8 +14,8 @@ export default class App extends Component {
     return (
       <Layout>
         <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
+        <Route path='/current' component={Counter} />
+        <Route path='/charts' component={FetchData} />
       </Layout>
     );
   }
--- WeatherStation/ClientApp/src/components/NavMenu.js
+++ WeatherStation/ClientApp/src/components/NavMenu.js
@@ -26,18 +26,15 @@ export class NavMenu extends Component {
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
           <Container>
-            <NavbarBrand tag={Link} to="/">WeatherStation</NavbarBrand>
+            <NavbarBrand tag={Link} to="/">Pi Sense Weather Station</NavbarBrand>
             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
+                  <NavLink tag={Link} className="text-dark" to="/">Current Weather</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+                  <NavLink tag={Link} className="text-dark" to="/counter">Charts</NavLink>
                 </NavItem>
               </ul>
             </Collapse>
